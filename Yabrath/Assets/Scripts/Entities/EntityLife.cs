212ff765fe710f381597870fe695a723e99ef41f
Siitoo <alfonsosanchez1896@gmail.com>
2020-02-05 18:11:26 +0100

    Enemy die

--- Yabrath/Assets/Scripts/Entities/EntityLife.cs
+++ Yabrath/Assets/Scripts/Entities/EntityLife.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EntityLife : MonoBehaviour
 {
     public int max_life = 100;
@@ -10,6 +10,8 @@ public class EntityLife : MonoBehaviour
     private int last_life = 0;
 
     public RectTransform rect = null;
+    private float max_width;
+    private bool need_update_hp;
     private Transform main_camera_transform = null;
 
     // Start is called before the first frame update
@@ -17,25 +19,33 @@ public class EntityLife : MonoBehaviour
     {
         main_camera_transform = Camera.main.transform;
         last_life = actual_life = max_life;
+        max_width = rect.transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
-        if(last_life != actual_life)
+        if (last_life != actual_life)
         {
-
+            need_update_hp = true;
         }
+        else
+            need_update_hp = false;
 
-        last_life = actual_life;
+        if (need_update_hp)
+            onUpdateHpBar();
+
         rect.LookAt(main_camera_transform);
     }
 
     public void onUpdateHpBar()
     {
-        
+        if (actual_life < 0)
+            actual_life = 0;
+
+        rect.GetChild(0).GetComponent<Slider>().value = (float)actual_life/max_life;
+
+        last_life = actual_life;
     }
 
     public void ReceiveDmg(int dmg)
