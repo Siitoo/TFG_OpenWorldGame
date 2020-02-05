340e13c3c85f2bed9ee6ecb2c3491adda760e5e6
Siitoo <alfonsosanchez1896@gmail.com>
2020-02-05 18:36:27 +0100

    Tumble/roll work

--- Yabrath/Assets/Scripts/Entities/MoveBehaviour.cs
+++ Yabrath/Assets/Scripts/Entities/MoveBehaviour.cs
@@ -16,6 +16,9 @@ public class MoveBehaviour : GenericBehaviour
 	private int groundedBool;                       // Animator variable related to whether or not the player is on ground.
 	private bool jump;                              // Boolean to determine whether or not the player started a jump.
 	private bool isColliding;                       // Boolean to determine if the player has collided with an obstacle.
+    private int tumbleBool;
+    private bool tumble;
+    private bool startTumble = false;
     private int strongAttackBool;
     private int resetComboBool;
     private bool start_jumping = false;
@@ -27,6 +30,7 @@ public class MoveBehaviour : GenericBehaviour
     private string[] strongAttackParameters;
     private string[] weakAttackParameters;
     private int combo_index = 0;
+    private float tumbleSpeed = 0.0f;
 	// Start is always called after any Awake functions.
 	void Start()
 	{
@@ -35,7 +39,7 @@ public class MoveBehaviour : GenericBehaviour
 		groundedBool = Animator.StringToHash("Grounded");
         strongAttackBool = Animator.StringToHash("StrongAttack");
         resetComboBool = Animator.StringToHash("ResetCombo");
-
+        tumbleBool = Animator.StringToHash("Tumble");
 		behaviourManager.GetAnim.SetBool(groundedBool, true);
         
 		// Subscribe and register this behaviour as the default behaviour.
@@ -61,12 +65,17 @@ public class MoveBehaviour : GenericBehaviour
 
         if(behaviourManager.inputController.StrongButton && behaviourManager.IsCurrentBehaviour(this.behaviourCode) && !behaviourManager.IsOverriding() && !jump)
         {
-                strongAttack = true;
+             strongAttack = true;
         }
 
         if (behaviourManager.inputController.WeakAttackButton && behaviourManager.IsCurrentBehaviour(this.behaviourCode) && !behaviourManager.IsOverriding() && !jump)
         {
             weakAttack = true;
+        }
+
+        if (behaviourManager.inputController.TumbleButton && behaviourManager.IsCurrentBehaviour(this.behaviourCode) && !behaviourManager.IsOverriding() && !jump)
+        {
+            tumble = true;
         }
     }
 
@@ -275,51 +284,7 @@ public class MoveBehaviour : GenericBehaviour
 
 
         }
-		// Start a new jump.
-		/*if (jump && !behaviourManager.GetAnim.GetBool(jumpBool) && behaviourManager.IsGrounded())
-		{
-			// Set jump related parameters.
-			behaviourManager.LockTempBehaviour(this.behaviourCode);
-			behaviourManager.GetAnim.SetBool(jumpBool, true);
-			// Is a locomotion jump?
-			if (behaviourManager.GetAnim.GetFloat(speedFloat) > 0.1)
-			{
-				// Temporarily change player friction to pass through obstacles.
-				GetComponent<CapsuleCollider>().material.dynamicFriction = 0f;
-				GetComponent<CapsuleCollider>().material.staticFriction = 0f;
-				// Remove vertical velocity to avoid "super jumps" on slope ends.
-				RemoveVerticalVelocity();
-				// Set jump vertical impulse velocity.
-				float velocity = 2f * Mathf.Abs(Physics.gravity.y) * jumpHeight;
-				velocity = Mathf.Sqrt(velocity);
-                behaviourManager.GetRigidBody.AddForce(Vector3.up * velocity *10.0f , ForceMode.VelocityChange);
-
-                //behaviourManager.GetRigidBody.AddRelativeForce(Vector3.up * velocity, ForceMode.VelocityChange);
-            }
-           // behaviourManager.GetRigidBody.velocity = Vector3.up * 40.0f;
-           
-        }
-		// Is already jumping?
-		else if (behaviourManager.GetAnim.GetBool(jumpBool))
-		{
-			// Keep forward movement while in the air.
-			if (!behaviourManager.IsGrounded() && !isColliding && behaviourManager.GetTempLockStatus())
-			{
-				behaviourManager.GetRigidBody.AddForce(transform.forward * jumpIntertialForce/4 * Physics.gravity.magnitude/10 * sprintSpeed, ForceMode.Acceleration);
-			}
-			// Has landed?
-			if ((behaviourManager.GetRigidBody.velocity.y < 0) && behaviourManager.IsGrounded())
-			{
-				behaviourManager.GetAnim.SetBool(groundedBool, true);
-				// Change back player friction to default.
-				GetComponent<CapsuleCollider>().material.dynamicFriction = 0.6f;
-				GetComponent<CapsuleCollider>().material.staticFriction = 0.6f;
-				// Set jump related parameters.
-				jump = false;
-				behaviourManager.GetAnim.SetBool(jumpBool, false);
-				behaviourManager.UnlockTempBehaviour(this.behaviourCode);
-			}
-		}*/
+		
 	}
 
 	// Deal with the basic player movement
@@ -332,34 +297,68 @@ public class MoveBehaviour : GenericBehaviour
         {
             behaviourManager.GetRigidBody.AddForce(Vector3.down * Mathf.Abs(Physics.gravity.y / 3), ForceMode.VelocityChange);
         }
-
-
 		// Avoid takeoff when reached a slope end.
 		else if (!behaviourManager.GetAnim.GetBool(jumpBool) && behaviourManager.GetRigidBody.velocity.y > 0)
 		{
 			RemoveVerticalVelocity();
 		}
 
-		// Call function that deals with player orientation.
-		Rotating(horizontal, vertical);
+        if (!tumble)
+        {
+            // Call function that deals with player orientation.
+            Rotating(horizontal, vertical);
 
-		// Set proper speed.
-		Vector2 dir = new Vector2(horizontal, vertical);
-		speed = Vector2.ClampMagnitude(dir, 1f).magnitude;
-		// This is for PC only, gamepads control speed via analog stick.
-		//speedSeeker += Input.GetAxis("Mouse ScrollWheel"); //SITO
-		speedSeeker = Mathf.Clamp(speedSeeker, walkSpeed, runSpeed);
-		speed *= speedSeeker;
-		if (behaviourManager.IsSprinting())
-		{
-			speed = sprintSpeed;
-		}
+            // Set proper speed.
+            Vector2 dir = new Vector2(horizontal, vertical);
+            speed = Vector2.ClampMagnitude(dir, 1f).magnitude;
+            // This is for PC only, gamepads control speed via analog stick.
+            //speedSeeker += Input.GetAxis("Mouse ScrollWheel"); //SITO
+            speedSeeker = Mathf.Clamp(speedSeeker, walkSpeed, runSpeed);
+            speed *= speedSeeker;
+            if (behaviourManager.IsSprinting())
+            {
+                speed = sprintSpeed;
+            }
 
-		behaviourManager.GetAnim.SetFloat(speedFloat, speed, speedDampTime, Time.deltaTime);
+            behaviourManager.GetAnim.SetFloat(speedFloat, speed, speedDampTime, Time.deltaTime);
 
-        // behaviourManager.GetRigidBody.AddForce(transform.forward*speed *10.0f,ForceMode.Impulse);
-        if(speed > 0.01f && behaviourManager.IsGrounded())
-            behaviourManager.GetRigidBody.MovePosition(transform.position + transform.forward * speed* Time.deltaTime);
+            // behaviourManager.GetRigidBody.AddForce(transform.forward*speed *10.0f,ForceMode.Impulse);
+            if (speed > 0.01f && behaviourManager.IsGrounded())
+                behaviourManager.GetRigidBody.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+        }
+        else
+        {
+
+            if(behaviourManager.IsGrounded() && !jump)
+            {
+                if(!startTumble)
+                {
+                    startTumble = true;
+                    behaviourManager.GetAnim.SetBool(tumbleBool, true);
+                    tumbleSpeed = behaviourManager.GetAnim.GetFloat(speedFloat);
+                }
+
+                if(startTumble && behaviourManager.GetAnim.GetCurrentAnimatorStateInfo(0).IsName("Tumble"))
+                {
+                    float tumbleMultiply = 1.5f;
+
+                    if (tumbleSpeed < 4.0f)
+                        tumbleMultiply = 3.0f;
+
+                    behaviourManager.GetRigidBody.MovePosition(transform.position + transform.forward * tumbleSpeed * tumbleMultiply * Time.deltaTime);
+
+                    if(behaviourManager.GetAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.8f)
+                    {
+                        startTumble = false;
+                        tumble = false;
+                        behaviourManager.GetAnim.SetBool(tumbleBool, false);
+                    }
+
+                }
+
+            }
+
+        }
     }
 
 	// Remove vertical rigidbody velocity.
