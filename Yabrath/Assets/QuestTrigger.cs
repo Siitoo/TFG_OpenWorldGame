82b733a5749886895a69a6a3e3586e2b2452d176
Siitoo <alfonsosanchez1896@gmail.com>
2020-04-14 10:03:42 +0200

    Added Quest Triggers

---
+++ Yabrath/Assets/QuestTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrigger : MonoBehaviour
+{
+    public int questId = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.FindGameObjectWithTag("Manager").GetComponent<QuestManager>().AddCurrent(questId, 1);
+
+        Destroy(this.gameObject);
+
+    }
+}
