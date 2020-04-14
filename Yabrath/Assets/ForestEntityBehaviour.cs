dbe9a52809137eece43b1e545c9fa508045b0d8e
Siitoo <alfonsosanchez1896@gmail.com>
2020-04-14 11:57:20 +0200

    Simple forest enemy

---
+++ Yabrath/Assets/ForestEntityBehaviour.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestEntityBehaviour : MonoBehaviour
+{
+    EntityLife life;
+    bool seePlayer = false;
+    bool attack = false;
+    bool die = false;
+
+    Animator anim;
+    Transform player_transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = gameObject.GetComponent<EntityLife>();
+        anim = gameObject.GetComponent<Animator>();
+        seePlayer = anim.GetBool("SeePlayer");
+        attack = anim.GetBool("AttackRange");
+        player_transform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(seePlayer && !die)
+        {
+            if(!attack)
+                Movement();
+
+            if (Mathf.Abs(Vector3.SqrMagnitude(transform.position - player_transform.position)) <= 1.0f)
+            {
+                attack = true;
+            }
+            else
+                attack = false;
+
+            anim.SetBool("AttackRange", attack);
+
+        }
+       
+        if(life.actual_life <= 0)
+        {
+            
+
+            if(!die)
+            {
+                anim.SetBool("Die", true);
+                StartCoroutine("DieTime");
+            }
+
+
+            die = true;
+        }
+       
+    }
+
+    void Movement()
+    {
+        transform.LookAt(player_transform);
+        transform.Translate(Vector3.forward*Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            seePlayer = true;
+            anim.SetBool("SeePlayer", seePlayer);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            seePlayer = true;
+            anim.SetBool("SeePlayer", seePlayer);
+        }
+    }
+
+    IEnumerator DieTime()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+    }
+
+}
--- Yabrath/Assets/Scripts/Entities/EntityLife.cs
+++ Yabrath/Assets/Scripts/Entities/EntityLife.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class EntityLife : MonoBehaviour
 {
     public int max_life = 100;
